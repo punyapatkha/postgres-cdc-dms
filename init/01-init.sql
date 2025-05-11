1cd93d7872aa5b921ea4f8fc7970d6bb47c72360
Your Name <you@example.com>
2025-05-11 12:54:56 +0700

    move to pg 17

--- init/01-init.sql
+++
@@ -1,6 +0,0 @@
--- Create a replication role for AWS DMS
-CREATE ROLE dms_replication WITH REPLICATION LOGIN PASSWORD 'dms_pass';
-
--- Optionally grant privileges
-GRANT CONNECT ON DATABASE cdc_db TO dms_replication;
-GRANT USAGE ON SCHEMA public TO dms_replication;
---
+++ init/init.sql
@@ -0,0 +1,15 @@
+-- -- Create a replication role for AWS DMS
+-- CREATE ROLE dms_replication WITH REPLICATION LOGIN PASSWORD 'dms_pass';
+
+-- -- Optionally grant privileges
+-- GRANT CONNECT ON DATABASE cdc_db TO dms_replication;
+-- GRANT USAGE ON SCHEMA public TO dms_replication;
+
+
+GRANT CONNECT ON DATABASE mydb TO dms_user;
+GRANT USAGE ON SCHEMA public TO dms_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO dms_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO dms_user;
+
+-- For replication (optional, only for ongoing replication tasks)
+ALTER ROLE dms_user WITH REPLICATION;
